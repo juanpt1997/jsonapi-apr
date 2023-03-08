6cba32b5b67411ecb55b2ed2794d0a78146a95c8
Juan Pablo Tabares Rico <tabaresricojuanpablo@utp.edu.co>
2023-03-07 23:37:32 -0700

    Created laravel resource for the response in show method of ArticleController

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ArticleResource;
 use App\Models\Article;
 use Illuminate\Http\Request;
 
@@ -11,20 +12,23 @@ class ArticleController extends Controller
     public function show(Article $article)
     {
         // return $article;
+        // ? Response expected
+        // return response()->json([
+        //     'data' => [
+        //         'type' => 'articles',
+        //         'id' => (string) $article->getRouteKey(),
+        //         'attributes' => [
+        //             'title' => $article->title,
+        //             'slug' => $article->slug,
+        //             'content' => $article->content,
+        //         ],
+        //         'links' => [
+        //             'self' => route('api.v1.articles.show', $article),
+        //         ]
+        //     ]
+        // ]);
 
-        return response()->json([
-            'data' => [
-                'type' => 'articles',
-                'id' => (string) $article->getRouteKey(),
-                'attributes' => [
-                    'title' => $article->title,
-                    'slug' => $article->slug,
-                    'content' => $article->content,
-                ],
-                'links' => [
-                    'self' => route('api.v1.articles.show', $article),
-                ]
-            ]
-        ]);
+        // Using ArticleResource
+        return ArticleResource::make($article);
     }
 }
---
+++ app/Http/Resources/ArticleResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ArticleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            // ? data is redundant because laravel resources actually returns the response encapsulated as data
+            // 'data' => [
+                'type' => 'articles',
+                'id' => (string) $this->resource->getRouteKey(),
+                'attributes' => [
+                    'title' => $this->resource->title,
+                    'slug' => $this->resource->slug,
+                    'content' => $this->resource->content,
+                ],
+                'links' => [
+                    'self' => route('api.v1.articles.show', $this->resource),
+                ]
+            // ]
+        ];
+    }
+}
